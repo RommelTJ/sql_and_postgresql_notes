dc9782d5d5a14b813276fe29c77e3c33ee296002
Rommel Rico <me@rommelrico.com>
2020-11-11 06:48:41 -0800

    :sparkles: Section2: More on the "Where" keyword

--- Section2/Section2.sql
+++ Section2/Section2.sql
@@ -8,3 +8,6 @@ CREATE TABLE cities (
 
 -- Where
 SELECT name, area FROM cities WHERE area > 4000;
+SELECT name, area FROM cities WHERE area = 8223;
+SELECT name, area FROM cities WHERE area <> 8223;
+SELECT name, area FROM cities WHERE area != 8223;
