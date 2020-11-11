9f2a07dc935396847d86f45e7806efc3da59cea8
Rommel Rico <me@rommelrico.com>
2020-11-11 06:53:18 -0800

    :sparkles: Section2: Compound "Where" Clauses

--- Section2/Section2.sql
+++ Section2/Section2.sql
@@ -11,3 +11,25 @@ SELECT name, area FROM cities WHERE area > 4000;
 SELECT name, area FROM cities WHERE area = 8223;
 SELECT name, area FROM cities WHERE area <> 8223;
 SELECT name, area FROM cities WHERE area != 8223;
+
+-- Compound where
+SELECT name, area FROM cities WHERE area BETWEEN 2000 AND 5000;
+SELECT name, area FROM cities WHERE name IN ('Delhi', 'Shanghai');
+SELECT name, area FROM cities WHERE name NOT IN ('Delhi', 'Shanghai');
+SELECT
+    name,
+    area
+FROM
+  cities
+WHERE
+  area NOT IN (3043, 8223)
+  AND name = 'Delhi';
+
+SELECT
+  name,
+  area
+FROM
+  cities
+WHERE
+  area NOT IN (3043, 8223)
+  OR name = 'Delhi';
