2f00d8d7d671f63615b4e53a55f8a853ccb62e18
Rommel Rico <me@rommelrico.com>
2020-11-11 06:44:50 -0800

    :sparkles: Section2: Filtering Rows with "Where"

---
+++ Section2/Section2.sql
@@ -0,0 +1,10 @@
+-- Create Table
+CREATE TABLE cities (
+                        name VARCHAR(50),
+                        country VARCHAR(50),
+                        population INTEGER,
+                        area INTEGER
+);
+
+-- Where
+SELECT name, area FROM cities WHERE area > 4000;
