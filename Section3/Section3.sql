fb1babf6aeb6c74a09f2e6258fc235291aa5baec
Rommel Rico <me@rommelrico.com>
2020-11-12 07:33:04 -0800

    :sparkles: Section3: Creating Foreign Key Columns

--- Section3/Section3.sql
+++ Section3/Section3.sql
@@ -7,3 +7,13 @@ CREATE TABLE users (
 -- Insert user
 INSERT INTO users (username) VALUES ('monahan93'), ('pfeffer'), ('99stroman'), ('si93onis');
 SELECT * FROM users;
+
+-- Creating photos (foreign key example)
+CREATE TABLE photos (
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(200),
+    user_id INTEGER REFERENCES users(id)
+);
+
+-- Insert photos
+INSERT INTO photos (url, user_id) VALUES ('http://one.jpg', 4);
