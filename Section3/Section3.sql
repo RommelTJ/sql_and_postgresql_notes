63023a6ef73fd28796fffd2df06643b81a262533
Rommel Rico <me@rommelrico.com>
2020-11-14 07:37:41 -0800

    :sparkles: Section3: Recreate commands

--- Section3/Section3.sql
+++ Section3/Section3.sql
@@ -38,3 +38,20 @@ INSERT INTO photos (url, user_id) VALUES ('http://jpg', NULL); -- Not tied to an
 
 -- Foreign Key Deleting Constraints
 DELETE FROM users WHERE id = 1; -- Error
+
+-- Recreate Commands
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id)
+);
+
+INSERT INTO photos (url, user_id)
+VALUES
+('http:/one.jpg', 4),
+('http:/two.jpg', 1),
+('http:/25.jpg', 1),
+('http:/36.jpg', 1),
+('http:/754.jpg', 2),
+('http:/35.jpg', 3),
+('http:/256.jpg', 4);
