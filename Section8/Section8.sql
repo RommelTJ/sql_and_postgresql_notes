6058b66fb20ec97bcf06deac639971c0322e337f
Rommel Rico <me@rommelrico.com>
2020-11-15 13:53:05 -0800

    :sparkles: Section8: Except keyword

--- Section8/Section8.sql
+++ Section8/Section8.sql
@@ -31,3 +31,8 @@ INTERSECT
 (SELECT * FROM products ORDER BY price DESC LIMIT 4)
 INTERSECT ALL
 (SELECT * FROM products ORDER BY (price/weight) DESC LIMIT 4);
+
+-- Except example
+(SELECT * FROM products ORDER BY price DESC LIMIT 4)
+EXCEPT
+(SELECT * FROM products ORDER BY (price/weight) DESC LIMIT 4);
