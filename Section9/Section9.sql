375c4e8f9c6a96e806c96686ff5dd74a35a3f8cc
Rommel Rico <me@rommelrico.com>
2020-11-16 07:23:18 -0800

    :sparkles: Section9: Subqueries in a Select

--- Section9/Section9.sql
+++ Section9/Section9.sql
@@ -35,3 +35,13 @@ WHERE price > (
 SELECT * FROM orders;
 SELECT id FROM orders;
 SELECT COUNT(*) FROM orders;
+
+-- Subqueries in a Select
+-- Subquery must result in a single value (one row, one column)
+SELECT name, price, (SELECT MAX(price) FROM products)
+FROM products
+WHERE price > 876;
+
+SELECT name, price, (SELECT price FROM products WHERE id = 3) AS id_3_price
+FROM products
+WHERE price > 876;
