121b740bba6f071c1749d7a877ea0383242457e9
Rommel Rico <me@rommelrico.com>
2020-11-20 07:43:03 -0800

    :sparkles: Section11: Greatest value in a list

--- Section11/Section11.sql
+++ Section11/Section11.sql
@@ -24,3 +24,9 @@ CREATE TABLE phones (
   units_sold INTEGER
 );
 
+-- The Greatest value in a list
+SELECT GREATEST(20, 10, 30); -- 30
+
+-- Compute the cost to ship each item
+-- Shipping is maximum of (weight * 2) or 30
+SELECT name, weight, GREATEST(30, weight * 2) AS cost_to_ship FROM products;
