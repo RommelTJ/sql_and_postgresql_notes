594b3e3cd7fd8c9e4c6983ea100936785bfde0da
Rommel Rico <me@rommelrico.com>
2020-11-20 07:45:03 -0800

    :sparkles: Section11: And the least value in a list

--- Section11/Section11.sql
+++ Section11/Section11.sql
@@ -30,3 +30,10 @@ SELECT GREATEST(20, 10, 30); -- 30
 -- Compute the cost to ship each item
 -- Shipping is maximum of (weight * 2) or 30
 SELECT name, weight, GREATEST(30, weight * 2) AS cost_to_ship FROM products;
+
+-- The least value in a list;
+SELECT LEAST(20, 10, 30); -- 10
+
+-- All products are on sale
+-- Price is the least of the product's price * 0.5 or $400.
+SELECT name, price, LEAST(400, price * 0.5) FROM products;
