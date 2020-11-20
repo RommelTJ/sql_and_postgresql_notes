467b389467527c0f862cf97cc25846761b44032f
Rommel Rico <me@rommelrico.com>
2020-11-20 07:48:52 -0800

    :sparkles: Section11: The case keyword

--- Section11/Section11.sql
+++ Section11/Section11.sql
@@ -37,3 +37,14 @@ SELECT LEAST(20, 10, 30); -- 10
 -- All products are on sale
 -- Price is the least of the product's price * 0.5 or $400.
 SELECT name, price, LEAST(400, price * 0.5) FROM products;
+
+-- Print each product and its price. Also print a description of the price.
+-- If price > 600, then 'high', If price > 300 'medium', else 'cheap'.
+SELECT
+  name,
+  price,
+  (CASE WHEN price > 600 THEN 'high'
+        WHEN price > 300 THEN 'medium'
+        ELSE 'cheap' END
+  ) AS description
+FROM products;
