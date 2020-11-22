5b7e89e512c03d7b55e5c74ebf258cf5079e5887
Rommel Rico <me@rommelrico.com>
2020-11-22 12:46:54 -0800

    :sparkles: Section20: Likes Creation

--- Section20/Section20.sql
+++ Section20/Section20.sql
@@ -35,13 +35,21 @@ CREATE TABLE posts(
   user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
--- table likes {
---   id SERIAL [pk, increment]
---   created_at timestamp
---   user_id integer [ref: > users.id]
---   post_id integer [ref: > posts.id]
---   comment_id integer [ref: > comments.id]
--- }
+-- Likes
+CREATE TABLE likes(
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+  comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE,
+  CHECK(
+    COALESCE((post_id)::BOOLEAN::INTEGER, 0)
+    +
+    COALESCE((comment_id)::BOOLEAN::INTEGER, 0)
+    = 1
+  ),
+  UNIQUE(user_id, post_id, comment_id)
+);
 
 -- table photo_tags {
 --   id SERIAL [pk, increment]
