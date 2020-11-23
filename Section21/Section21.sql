d5277303f821e269cd37128ed2d66745c5475afd
Rommel Rico <me@rommelrico.com>
2020-11-23 07:18:50 -0800

    :sparkles: Section21: Likes per User

--- Section21/Section21.sql
+++ Section21/Section21.sql
@@ -4,3 +4,10 @@ SELECT id FROM users ORDER BY id DESC LIMIT 3;
 -- Posts by a Particular User
 -- Join users and posts table. Show username of user id 200 and captions of all posts they have created
 SELECT username, caption FROM users JOIN posts ON (users.id = posts.user_id) WHERE users.id = 200;
+
+-- Likes per User
+-- Show each username and the number of likes that they have created
+SELECT username, COUNT(*) AS num_of_likes
+FROM users
+JOIN likes ON (likes.user_id = users.id)
+GROUP BY username;
