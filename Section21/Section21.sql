a6e53641420ea605049e91344a6ef44066eddbc1
Rommel Rico <me@rommelrico.com>
2020-11-23 07:14:43 -0800

    :sparkles: Section21: Posts by a particular user

--- Section21/Section21.sql
+++ Section21/Section21.sql
@@ -1,2 +1,6 @@
 -- Highest user id's exercise
 SELECT id FROM users ORDER BY id DESC LIMIT 3;
+
+-- Posts by a Particular User
+-- Join users and posts table. Show username of user id 200 and captions of all posts they have created
+SELECT username, caption FROM users JOIN posts ON (users.id = posts.user_id) WHERE users.id = 200;
